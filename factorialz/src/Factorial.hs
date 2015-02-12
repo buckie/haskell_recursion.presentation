c443f0d0709526fd094cbc8fdc66ae47d8d4b9e3
buckie <wjmartino@gmail.com>
2015-02-12 14:10:49 -0500

    not sure whats happening
    Signed-off-by: buckie <wjmartino@gmail.com>

--- factorialz/src/Factorial.hs
+++ factorialz/src/Factorial.hs
@@ -1,11 +1,23 @@
+{-# LANGUAGE BangPatterns, MagicHash, UnboxedTuples #-}
+
 module Factorial where
 
-factorial :: Integer -> Integer
+import Data.Int
+
+factorial :: (Num a, Eq a) => a -> a
 factorial 0 = 1
-factorial n = n * factorial(n-1)
+factorial n = n * factorial (n-1)
 
-facc :: Integer -> Integer -> Integer
-facc 0 a = a
-facc n a = facc (n-1) (a*n)
+factorial' :: (Num a, Eq a) => a -> a
+factorial' 0 = 1
+factorial' n = (factorial (n-1)) * n
+
+factorial'' :: (Num a, Eq a) => a -> a
+factorial'' n = if n /= 0
+                  then n * factorial (n-1)
+                  else 1
 
 
+fac :: (Num a, Eq a) => a -> a -> a
+fac a 0 = a
+fac a n = fac (n*a) (n-1)
--- factorialz/src/Main.hs
+++ factorialz/src/Main.hs
@@ -1,16 +1,18 @@
 module Main where
 
 import Criterion.Main
-
+import Data.Int
 import Factorial
 
 benchF :: (Show a) => (a -> b) -> a -> Benchmark
 benchF f x = bench (show x) $ whnf f x
 
 inputs :: [Integer]
-inputs =  drop 13 $ take 17 $ iterate (*2) 1
+inputs = [10..13]
 
 main :: IO ()
-main = defaultMain [ bgroup "factorial" $ map (benchF factorial) inputs
-                   , bgroup "facc" $ map (benchF (`facc` 1)) inputs
+main = defaultMain [ bgroup "factorial" $ fmap (benchF factorial) inputs
+                   , bgroup "factorial backwards" $ fmap (benchF factorial') inputs
+                   , bgroup "factorial with if" $ fmap (benchF factorial'') inputs
+                   , bgroup "fac" $ fmap (benchF (fac 1)) inputs
                    ]
