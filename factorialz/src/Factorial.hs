ecfd8ee4bab1eaacc869cc0abc7be57a49d3bdcf
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2015-02-12 20:24:27 -0500

    doing this makes it slower

--- factorialz/src/Factorial.hs
+++ factorialz/src/Factorial.hs
@@ -5,23 +5,27 @@ module Factorial where
 import Data.Int
 -- import  GHC.TypeLits
 
+{-# NOINLINE factorial #-}
 factorial :: (Num a, Eq a) => a -> a
 factorial 0 = 1
 factorial n = n * factorial (n-1)
 
+{-# NOINLINE factorial_Int #-}
 factorial_Int :: Int -> Int
 factorial_Int 0 = 1
 factorial_Int n = n * factorial_Int (n-1 )
 
+{-# NOINLINE factorial_Integer #-}
 factorial_Integer :: Integer -> Integer
 factorial_Integer 0 = 1
 factorial_Integer n = n * factorial_Integer (n-1 )
 
-
+{-# NOINLINE fac #-}
 fac :: (Num a, Eq a) => a -> a -> a
 fac a 0 = a
 fac a n = fac (n*a) (n-1)
 
+{-# NOINLINE fac_strict_int #-}
 fac_strict_int :: Int -> Int
 fac_strict_int n = go 0 n
   where
@@ -29,7 +33,7 @@ fac_strict_int n = go 0 n
     go !accum !0 = accum
     go !accum !n = go (n* accum) (n-1)
 
-
+{-# NOINLINE fac_strict_integer #-}
 fac_strict_integer :: Integer -> Integer
 fac_strict_integer n = go 0 n
   where
