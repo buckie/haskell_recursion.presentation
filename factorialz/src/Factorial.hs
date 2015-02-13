e2b15df2419ec2325b6d47250c6ed8d76655c0fa
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2015-02-12 20:19:00 -0500

    more infos

--- factorialz/src/Factorial.hs
+++ factorialz/src/Factorial.hs
@@ -3,21 +3,36 @@
 module Factorial where
 
 import Data.Int
+-- import  GHC.TypeLits
 
 factorial :: (Num a, Eq a) => a -> a
 factorial 0 = 1
 factorial n = n * factorial (n-1)
 
-factorial' :: (Num a, Eq a) => a -> a
-factorial' 0 = 1
-factorial' n = (factorial (n-1)) * n
+factorial_Int :: Int -> Int
+factorial_Int 0 = 1
+factorial_Int n = n * factorial_Int (n-1 )
 
-factorial'' :: (Num a, Eq a) => a -> a
-factorial'' n = if n /= 0
-                  then n * factorial (n-1)
-                  else 1
+factorial_Integer :: Integer -> Integer
+factorial_Integer 0 = 1
+factorial_Integer n = n * factorial_Integer (n-1 )
 
 
 fac :: (Num a, Eq a) => a -> a -> a
 fac a 0 = a
 fac a n = fac (n*a) (n-1)
+
+fac_strict_int :: Int -> Int
+fac_strict_int n = go 0 n
+  where
+    go :: Int -> Int -> Int
+    go !accum !0 = accum
+    go !accum !n = go (n* accum) (n-1)
+
+
+fac_strict_integer :: Integer -> Integer
+fac_strict_integer n = go 0 n
+  where
+    go :: Integer -> Integer -> Integer
+    go !accum !0 = accum
+    go !accum !n = go (n* accum) (n-1)
--- factorialz/src/Main.hs
+++ factorialz/src/Main.hs
@@ -1,18 +1,25 @@
 module Main where
 
 import Criterion.Main
-import Data.Int
 import Factorial
 
 benchF :: (Show a) => (a -> b) -> a -> Benchmark
 benchF f x = bench (show x) $ whnf f x
 
-inputs :: [Integer]
-inputs = [10..13]
+inputs_integer :: [Integer]
+inputs_integer = [10..13]
+
+inputs_int :: [Int]
+inputs_int = [10..13]
 
 main :: IO ()
-main = defaultMain [ bgroup "factorial" $ fmap (benchF factorial) inputs
-                   , bgroup "factorial backwards" $ fmap (benchF factorial') inputs
-                   , bgroup "factorial with if" $ fmap (benchF factorial'') inputs
-                   , bgroup "fac" $ fmap (benchF (fac 1)) inputs
+main = defaultMain [ bgroup "generic factorial Integer" $ fmap (benchF factorial) inputs_integer
+                   , bgroup "integer specialized factorial" $ fmap (benchF factorial_Integer) inputs_integer
+                   , bgroup "generic lazy fac int" $ fmap (benchF (fac 1)) inputs_integer
+                   , bgroup "integer strict fact" $ fmap (benchF (fac_strict_integer )) inputs_integer
+
+                   ,bgroup "generic factorial Int" $ fmap (benchF factorial) inputs_int
+                   , bgroup "int specialized factorial" $ fmap (benchF factorial_Int) inputs_int
+                   , bgroup "generic lazy fac int" $ fmap (benchF (fac 1)) inputs_int
+                   , bgroup "integer strict fact" $ fmap (benchF (fac_strict_int )) inputs_int
                    ]
